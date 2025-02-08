e20b79d937f4deae887a0005df8b21c3f487af4c
Kevin Huertas <kevindhuertas@gmail.com>
2025-02-08 12:30:06 -0500

    Email notification implemented and macOs driver

--- cita_checker.py
+++ cita_checker.py
@@ -1,22 +1,34 @@
 from selenium import webdriver
+import platform
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
-from userCredentials import USER_EMAIL, USER_PASSWORD
+from email_alert import EmailAlert
+from page_credentials import USER_EMAIL, USER_PASSWORD
 
 url = "https://ais.usvisa-info.com/es-ec/niv/schedule/52492462/appointment"
+alert_sent = False
+location = '';
 
 def verificar_cita():
     chrome_options = Options()
     # chrome_options.add_argument("--headless")  # Ejecuta el navegador en modo headless (sin interfaz gráfica)
-    # chrome_options.add_argument("--disable-gpu")
-    # chrome_options.add_argument("--no-sandbox")
+    # chrome_options.add_argument("--disable-gpu")  # Necesario para algunos entornos headless
+    # chrome_options.add_argument("--no-sandbox")  # Evita errores en contenedores
+    # chrome_options.add_argument("--disable-dev-shm-usage")  # Mejora rendimiento en modo headless
 
+    # Detectar sistema operativo
+    if platform.system() == "Windows":
+        driver_path = "./chromedriver.exe"
+    elif platform.system() == "Darwin":  # macOS
+        driver_path = "./chromedriver"
+    else:
+        raise Exception("Sistema operativo no soportado. Solo Windows y macOS son compatibles.")
 
-    driver_path = "./chromedriver.exe"
+    # Configurar el servicio de ChromeDriver
     service = Service(driver_path)
 
     driver = webdriver.Chrome(service=service, options=chrome_options)
@@ -46,6 +58,7 @@ def verificar_cita():
         errorController(e)
     finally:
         driver.quit()
+
 
 
 
@@ -81,25 +94,38 @@ def login(driver):
         errorController(f"Error durante el proceso de inicio de sesión: {e}")
 
 def check_dates(driver):
+    global alert_sent,location
     try:
         # Variables
         location_select_id = "appointments_consulate_appointment_facility_id"
         date_input_id = "appointments_consulate_appointment_date"
         first_group_class = "ui-datepicker-group-first"
         next_button_class = "ui-datepicker-next"
-        stop_month = ["June", "Junio"]
+        # stop_month = ["June", "Junio"]
+        stop_month = ["September", "Septiembre"]
 
+        # First location
         location_select = WebDriverWait(driver, 10).until(
             EC.presence_of_element_located((By.ID, location_select_id))
         )
+        location = "Quito";
         location_select.send_keys("Quito")
         time.sleep(3)  
         verify_dates_until_june(driver, date_input_id, first_group_class, next_button_class, stop_month)
 
-
-        print("Cambiando a la ubicación Guayaquil...")
-        location_select.send_keys("Guayaquil")
+        # reset date_pciker and alert
+        alert_sent = False
+        location = "Guayaquil";
+        location_select = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.ID, location_select_id))
+        )
         driver.find_element(By.TAG_NAME, "body").click()
+        driver.find_element(By.ID, "header").click()
+        
+        # second location
+        print("Cambiando a la ubicación Guayaquil...")
+        location_select.send_keys(location)
+
         time.sleep(5) 
         verify_dates_until_june(driver, date_input_id, first_group_class, next_button_class, stop_month)
 
@@ -116,12 +142,13 @@ def verify_dates_until_june(driver, date_input_id, first_group_class, next_butto
             driver.execute_script("arguments[0].click();", date_input)
 
             check_calendar_group(driver, first_group_class)
+            if(alert_sent): break
             first_group = driver.find_element(By.CLASS_NAME, first_group_class)
             current_month = first_group.find_element(By.CLASS_NAME, "ui-datepicker-month").text
             if current_month in stop_month:
                 print(f"Mes de {current_month} alcanzado, terminando la verificación.")
                 break
-
+            
             # Pasar al siguiente mes (doble clic en el botón "next" para asegurarnos de avanzar mes a mes)
             next_button = WebDriverWait(driver, 10).until(
                 EC.element_to_be_clickable((By.CLASS_NAME, next_button_class))
@@ -155,4 +182,10 @@ def check_calendar_group(driver, group_class):
         errorController(f"Error durante la verificación del grupo de calendario: {e}")
 
 def alert_available_date(month, day):
+    global alert_sent
     print(f"Fecha disponible encontrada: {day} de {month}")
+    if(alert_sent == False):
+        alert_system = EmailAlert()
+        alert_system.send_email_alert(month, day,location)
+        alert_sent = True
+    
---
+++ email_alert.py
@@ -0,0 +1,44 @@
+import smtplib
+from email.message import EmailMessage
+from gmail_credentials import SENDER_EMAIL, PASSWORD, RECIPIENT_EMAIL
+
+class EmailAlert:
+    def __init__(self):
+        """
+        Inicializa el sistema de alertas con credenciales desde archivo
+        """
+        self.sender_email = SENDER_EMAIL
+        self.sender_password = PASSWORD
+        self.recipient_email = RECIPIENT_EMAIL
+
+    def send_email_alert(self, month: str, day: str, location: str) -> None:
+        """
+        Envía alerta de cita disponible por email
+        """
+        msg = EmailMessage()
+        msg['Subject'] = f"CITA DISPONIBLE el {day} de {month} en {location}"
+        msg['From'] = self.sender_email
+        msg['To'] = self.recipient_email
+        msg.set_content(
+            f"¡Se ha detectado una cita disponible!\n\n"
+            f"Fecha: {day} de {month}\n"
+            f"Ubicación: {location}\n\n"
+            "Accede rápidamente a reservarla antes de que se agote."
+        )
+
+        # Enviar email con conexión segura
+        try:
+            with smtplib.SMTP_SSL('smtp.gmail.com', 465) as server:
+                server.login(self.sender_email, self.sender_password)
+                server.send_message(msg)
+                print("Alerta enviada exitosamente!")
+        except Exception as e:
+            print(f"Error enviando email: {str(e)}")
+
+# if __name__ == "__main__":
+#     alert_system = EmailAlert()
+#     alert_system.send_email_alert(
+#         month="Julio",
+#         day="15",
+#         location="Madrid Centro"
+#     )
